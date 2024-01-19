4e56613ab59bcb5d18990223e3f699426a6bb5ed
Nayef Alzayani <nzayani57@outlook.com>
2024-01-19 19:27:14 +0300

    game tells you if you win

--- script.js
+++ script.js
@@ -8,34 +8,21 @@ const yellowBlock = document.querySelectorAll('.yellow')
 const orangeBlock = document.querySelectorAll('.orange')
 const tiles = document.querySelectorAll('.block')
 
-const redTiles = [
-  tiles[5].classList.contains('red'),
-  tiles[10].classList.contains('red'),
-  tiles[15].classList.contains('red'),
-  tiles[20].classList.contains('red')
-]
-
 const redTilesMatch = []
-const winningCondition = () => {
-  if (redTilesMatch.length === 4) {
-    console.log('red complete')
-  }
-}
+const blueTilesMatch = []
+const greenTilesMatch = []
+const yellowTilesMatch = []
+const orangeTilesMatch = []
 
-console.log(redTiles)
-
-redTiles.forEach((el) => {})
-
-// redTiles.forEach((el) => {
-//   if (redTiles[el].classList.obtains('red')) {
-//     redTilesMatch.push(1)
-//   }
-// })
-
-for (i = 0; i < redTiles.length; i++) {
-  if (redTiles[i] === true) {
-    redTilesMatch.push(1)
-    console.log(redTilesMatch)
+const winCondition = () => {
+  if (
+    redTilesMatch.length === 4 &&
+    blueTilesMatch.length === 3 &&
+    yellowTilesMatch.length === 2 &&
+    orangeTilesMatch.length === 3 &&
+    greenTilesMatch.length === 3
+  ) {
+    alert('you have won')
   }
 }
 
@@ -49,21 +36,32 @@ const gamePlay = () => {
       else if (e.target.classList.contains('orange')) return
 
       e.target.classList.add(`${colorChooser}`)
-      for (i = 0; i < redTiles.length; i++) {
-        if (redTiles[i] === true) {
-          redTilesMatch.push(1)
-          console.log(redTilesMatch)
-        }
+
+      if (e.target.classList.contains('red')) {
+        redTilesMatch.push(1)
+        console.log(redTilesMatch)
+        winCondition()
+      } else if (e.target.classList.contains('blue')) {
+        blueTilesMatch.push(1)
+        winCondition()
+      } else if (e.target.classList.contains('green')) {
+        greenTilesMatch.push(1)
+        winCondition()
+      } else if (e.target.classList.contains('yellow')) {
+        yellowTilesMatch.push(1)
+        winCondition()
+      } else if (e.target.classList.contains('orange')) {
+        orangeTilesMatch.push(1)
+        winCondition()
       }
-      winningCondition()
     })
 }
-
 for (i = 0; i < redBlock.length; i++) {
   redBlock[i].addEventListener('click', () => {
     colorChooser = 'red'
     console.log(colorChooser)
     gamePlay()
+    //winCondition()
   })
 }
 
@@ -72,6 +70,7 @@ blueBlock.forEach((el) => {
     colorChooser = 'blue'
     console.log(colorChooser)
     gamePlay()
+    //winCondition()
   })
 })
 
@@ -80,6 +79,7 @@ greenBlock.forEach((el) => {
     colorChooser = 'green'
     console.log(colorChooser)
     gamePlay()
+    //winCondition()
   })
 })
 yellowBlock.forEach((el) => {
@@ -87,6 +87,7 @@ yellowBlock.forEach((el) => {
     colorChooser = 'yellow'
     console.log(colorChooser)
     gamePlay()
+    //winCondition()
   })
 })
 orangeBlock.forEach((el) => {
@@ -94,5 +95,18 @@ orangeBlock.forEach((el) => {
     colorChooser = 'orange'
     console.log(colorChooser)
     gamePlay()
+    //winCondition()
   })
 })
+// redTiles.forEach((el) => {
+//   if (redTiles[el].classList.obtains('red')) {
+//     redTilesMatch.push(1)
+//   }
+// })
+
+// for (i = 0; i < redTiles.length; i++) {
+//   if (redTiles[i] === true) {
+//     redTilesMatch.push(1)
+//     console.log(redTilesMatch)
+//   }
+// }
