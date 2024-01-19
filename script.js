83de78dca117d2ba64736dc8c3741c8d0ee5f4e5
Nayef Alzayani <nzayani57@outlook.com>
2024-01-19 16:28:32 +0300

    winning condition for 1 color complete

--- script.js
+++ script.js
@@ -3,20 +3,62 @@ const blockSelected = []
 
 const redBlock = document.querySelectorAll('.red')
 const blueBlock = document.querySelectorAll('.blue')
+const greenBlock = document.querySelectorAll('.green')
+const yellowBlock = document.querySelectorAll('.yellow')
+const orangeBlock = document.querySelectorAll('.orange')
 const tiles = document.querySelectorAll('.block')
 
-for (i = 0; i < 9; i++) {
-  blockSelected.push(1)
-} // add the number of blocks to the array
+const redTiles = [
+  tiles[5].classList.contains('red'),
+  tiles[10].classList.contains('red'),
+  tiles[15].classList.contains('red'),
+  tiles[20].classList.contains('red')
+]
+
+const redTilesMatch = []
+const winningCondition = () => {
+  if (redTilesMatch.length === 4) {
+    console.log('red complete')
+  }
+}
+
+console.log(redTiles)
+
+redTiles.forEach((el) => {})
+
+// redTiles.forEach((el) => {
+//   if (redTiles[el].classList.obtains('red')) {
+//     redTilesMatch.push(1)
+//   }
+// })
+
+for (i = 0; i < redTiles.length; i++) {
+  if (redTiles[i] === true) {
+    redTilesMatch.push(1)
+    console.log(redTilesMatch)
+  }
+}
 
 const gamePlay = () => {
   for (i = 0; i < tiles.length; i++)
     tiles[i].addEventListener('click', (e) => {
       if (e.target.classList.contains('red')) return
       else if (e.target.classList.contains('blue')) return
+      else if (e.target.classList.contains('green')) return
+      else if (e.target.classList.contains('yellow')) return
+      else if (e.target.classList.contains('orange')) return
+
       e.target.classList.add(`${colorChooser}`)
+      for (i = 0; i < redTiles.length; i++) {
+        if (redTiles[i] === true) {
+          redTilesMatch.push(1)
+          console.log(redTilesMatch)
+        }
+      }
+      winningCondition()
     })
 }
+
 for (i = 0; i < redBlock.length; i++) {
   redBlock[i].addEventListener('click', () => {
     colorChooser = 'red'
@@ -33,20 +75,24 @@ blueBlock.forEach((el) => {
   })
 })
 
-// const T2 = document.querySelector('#T2')
-// const T3 = document.querySelector('#T3')
-// const T4 = document.querySelector('#T4')
-// const T5 = document.querySelector('#T5')
-// const T6 = document.querySelector('#T6')
-
-//   T3.addEventListener('click', () => {
-//     if (blockSelected[2] === 0) return
-//     T3.classList.add(`${colorChooser}`)
-//     blockSelected[2] = 0
-//   })
-//   T5.addEventListener('click', () => {
-//     if (blockSelected[4] === 0) return
-//     T5.classList.add(`${colorChooser}`)
-//     blockSelected[4] = 0
-//   })
-//
+greenBlock.forEach((el) => {
+  el.addEventListener('click', () => {
+    colorChooser = 'green'
+    console.log(colorChooser)
+    gamePlay()
+  })
+})
+yellowBlock.forEach((el) => {
+  el.addEventListener('click', () => {
+    colorChooser = 'yellow'
+    console.log(colorChooser)
+    gamePlay()
+  })
+})
+orangeBlock.forEach((el) => {
+  el.addEventListener('click', () => {
+    colorChooser = 'orange'
+    console.log(colorChooser)
+    gamePlay()
+  })
+})
